4e502d210dab9b0e83eacb90934528629d6e51b0
Slokh <slokhx@gmail.com>
2024-01-30 16:43:03 -0500

    feat(app): add app package

---
+++ packages/app/app/(tabs)/two.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'tamagui'
+import { ButtonDemo } from '../../components/button'
+
+export default function TabTwoScreen() {
+  return (
+    <View flex={1} alignItems="center">
+      <ButtonDemo />
+    </View>
+  )
+}
