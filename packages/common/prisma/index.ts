779433b487d6fa927935d76eac34270276f2e1dc
Slokh <slokhx@gmail.com>
2024-02-01 09:14:35 -0500

    fix(common): remove declaration

--- packages/common/prisma/index.ts
+++ packages/common/prisma/index.ts
@@ -1,13 +1,3 @@
 import * as farcaster from "./farcaster";
 import * as relations from "./relations";
 export { farcaster, relations };
-declare global {
-  type FarcasterCastMention = {
-    hash: string;
-    fid: bigint;
-    mention: bigint;
-    mentionPosition: bigint;
-    timestamp: Date;
-    deletedAt?: Date;
-  };
-}
