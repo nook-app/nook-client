2d1cdb7370a457607419655bf9335d4b386c4e8c
James Wenzel <wenzel.james.r@gmail.com>
2024-02-08 16:36:06 -0800

    support button targets and aspect ratios

--- packages/content/__test__/handlers/url.test.ts
+++ packages/content/__test__/handlers/url.test.ts
@@ -9,23 +9,28 @@ describe("@flink/content/handlers/url", () => {
   test("resolves full frame metadata", async () => {
     global.fetch = jest.fn().mockResolvedValue({
       text: () => `
-        <head>
-        <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="image" />
-        <meta property="fc:frame:post_url" content="post_url" />
-        <meta property="fc:frame:button:1" content="button1" />
-        <meta property="fc:frame:button:1:action" content="button1_action" />
-        <meta property="fc:frame:button:2" content="button2" />
-        <meta property="fc:frame:button:2:action" content="button2_action" />
-        <meta property="fc:frame:button:3" content="button3" />
-        <meta property="fc:frame:button:3:action" content="button3_action" />
-        <meta property="fc:frame:button:4" content="button4" />
-        <meta property="fc:frame:button:4:action" content="button4_action" />
-        <meta property="fc:frame:refresh_period" content="refresh_period" />
-        <meta property="fc:frame:idem_key" content="idem_key" />
-        <meta property="fc:frame:fake_idk" content="fake" />
-        <meta property="fc:frame:input:text" content="text_input" />
-        </head>
+      <head>
+      <meta name="fc:frame" content="vNext" />
+      <meta name="fc:frame:image" content="image" />
+      <meta name="fc:frame:post_url" content="post_url" />
+      <meta name="fc:frame:button:1" content="button1" />
+      <meta name="fc:frame:button:1:action" content="button1_action" />
+      <meta name="fc:frame:button:1:target" content="button1_target" />
+      <meta name="fc:frame:button:2" content="button2" />
+      <meta name="fc:frame:button:2:action" content="button2_action" />
+      <meta name="fc:frame:button:2:target" content="button2_target" />
+      <meta name="fc:frame:button:3" content="button3" />
+      <meta name="fc:frame:button:3:action" content="button3_action" />
+      <meta name="fc:frame:button:3:target" content="button3_target" />
+      <meta name="fc:frame:button:4" content="button4" />
+      <meta name="fc:frame:button:4:action" content="button4_action" />
+      <meta name="fc:frame:button:4:target" content="button4_target" />
+      <meta name="fc:frame:refresh_period" content="refresh_period" />
+      <meta name="fc:frame:idem_key" content="idem_key" />
+      <meta name="fc:frame:fake_idk" content="fake" />
+      <meta name="fc:frame:input:text" content="text_input"/>
+      <meta name="fc:frame:image:aspect_ratio" content="1:1"/>
+      </head>
         `,
       headers: {
         get: (key: string) => {
@@ -40,7 +45,7 @@ describe("@flink/content/handlers/url", () => {
         },
       },
     });
-    const result = await fetchUrlMetadata("https://flink.fyi");
+    const result = await fetchUrlMetadata("https://flink.help");
     expect(result.contentLength).toBe(100);
     expect(result.contentType).toBe("text/html");
     // @ts-ignore
@@ -52,27 +57,94 @@ describe("@flink/content/handlers/url", () => {
         {
           label: "button1",
           action: "button1_action",
+          target: "button1_target",
           index: 1,
         },
         {
           label: "button2",
           action: "button2_action",
+          target: "button2_target",
           index: 2,
         },
         {
           label: "button3",
           action: "button3_action",
+          target: "button3_target",
           index: 3,
         },
         {
           label: "button4",
           action: "button4_action",
+          target: "button4_target",
           index: 4,
         },
       ],
       refreshPeriod: undefined,
       idempotencyKey: "idem_key",
       textInput: "text_input",
+      aspectRatio: "1:1",
+    });
+  });
+
+  test("resolves frame metadata with fewer than 4 buttons", async () => {
+    // same as above but with fewer buttons
+    global.fetch = jest.fn().mockResolvedValue({
+      text: () => `
+      <head>
+      <meta name="fc:frame" content="vNext" />
+      <meta name="fc:frame:image" content="image" />
+      <meta name="fc:frame:post_url" content="post_url" />
+      <meta name="fc:frame:button:1" content="button1" />
+      <meta name="fc:frame:button:1:action" content="button1_action" />
+      <meta name="fc:frame:button:1:target" content="button1_target" />
+      <meta name="fc:frame:button:2" content="button2" />
+      <meta name="fc:frame:button:2:action" content="button2_action" />
+      <meta name="fc:frame:button:2:target" content="button2_target" />
+      <meta name="fc:frame:refresh_period" content="refresh_period" />
+      <meta name="fc:frame:idem_key" content="idem_key" />
+      <meta name="fc:frame:fake_idk" content="fake" />
+      <meta name="fc:frame:input:text" content="text_input"/>
+      <meta name="fc:frame:image:aspect_ratio" content="1:1"/>
+      </head>
+        `,
+      headers: {
+        get: (key: string) => {
+          switch (key) {
+            case "content-type":
+              return "text/html";
+            case "content-length":
+              return "100";
+            default:
+              return "";
+          }
+        },
+      },
+    });
+    const result = await fetchUrlMetadata("https://flink.help");
+    expect(result.contentLength).toBe(100);
+    expect(result.contentType).toBe("text/html");
+    // @ts-ignore
+    expect(result.metadata.frame).toEqual({
+      version: "vNext",
+      image: "image",
+      postUrl: "post_url",
+      buttons: [
+        {
+          label: "button1",
+          action: "button1_action",
+          target: "button1_target",
+          index: 1,
+        },
+        {
+          label: "button2",
+          action: "button2_action",
+          target: "button2_target",
+          index: 2,
+        },
+      ],
+      idempotencyKey: "idem_key",
+      textInput: "text_input",
+      aspectRatio: "1:1",
     });
   });
 });
--- packages/content/__test__/utils/metascraper-frame.test.ts
+++ packages/content/__test__/utils/metascraper-frame.test.ts
@@ -39,16 +39,21 @@ describe("metascraperFrame", () => {
     <meta property="fc:frame:post_url" content="post_url" />
     <meta property="fc:frame:button:1" content="button1" />
     <meta property="fc:frame:button:1:action" content="button1_action" />
+    <meta property="fc:frame:button:1:target" content="button1_target" />
     <meta property="fc:frame:button:2" content="button2" />
     <meta property="fc:frame:button:2:action" content="button2_action" />
+    <meta property="fc:frame:button:2:target" content="button2_target" />
     <meta property="fc:frame:button:3" content="button3" />
     <meta property="fc:frame:button:3:action" content="button3_action" />
+    <meta property="fc:frame:button:3:target" content="button3_target" />
     <meta property="fc:frame:button:4" content="button4" />
     <meta property="fc:frame:button:4:action" content="button4_action" />
+    <meta property="fc:frame:button:4:target" content="button4_target" />
     <meta property="fc:frame:refresh_period" content="refresh_period" />
     <meta property="fc:frame:idem_key" content="idem_key" />
     <meta property="fc:frame:fake_idk" content="fake" />
     <meta property="fc:frame:input:text" content="text_input"/>
+    <meta property="fc:frame:image:aspect_ratio" content="1:1"/>
     </head>
     `;
     const dom = cheerio.load(fullFrameHtml);
@@ -59,15 +64,69 @@ describe("metascraperFrame", () => {
     checkRule(frame, dom, "framePostUrl", "post_url");
     checkRule(frame, dom, "frameButton1", "button1");
     checkRule(frame, dom, "frameButton1Action", "button1_action");
+    checkRule(frame, dom, "frameButton1Target", "button1_target");
     checkRule(frame, dom, "frameButton2", "button2");
     checkRule(frame, dom, "frameButton2Action", "button2_action");
+    checkRule(frame, dom, "frameButton2Target", "button2_target");
     checkRule(frame, dom, "frameButton3", "button3");
     checkRule(frame, dom, "frameButton3Action", "button3_action");
+    checkRule(frame, dom, "frameButton3Target", "button3_target");
     checkRule(frame, dom, "frameButton4", "button4");
     checkRule(frame, dom, "frameButton4Action", "button4_action");
+    checkRule(frame, dom, "frameButton4Target", "button4_target");
     checkRule(frame, dom, "frameRefreshPeriod", "refresh_period");
     checkRule(frame, dom, "frameIdemKey", "idem_key");
     checkRule(frame, dom, "frameTextInput", "text_input");
+    checkRule(frame, dom, "frameImageAspectRatio", "1:1");
+  });
+
+  test("test nonstandard 'name' attribute", () => {
+    const fullFrameHtml = `
+    <head>
+    <meta name="fc:frame" content="vNext" />
+    <meta name="fc:frame:image" content="image" />
+    <meta name="fc:frame:post_url" content="post_url" />
+    <meta name="fc:frame:button:1" content="button1" />
+    <meta name="fc:frame:button:1:action" content="button1_action" />
+    <meta name="fc:frame:button:1:target" content="button1_target" />
+    <meta name="fc:frame:button:2" content="button2" />
+    <meta name="fc:frame:button:2:action" content="button2_action" />
+    <meta name="fc:frame:button:2:target" content="button2_target" />
+    <meta name="fc:frame:button:3" content="button3" />
+    <meta name="fc:frame:button:3:action" content="button3_action" />
+    <meta name="fc:frame:button:3:target" content="button3_target" />
+    <meta name="fc:frame:button:4" content="button4" />
+    <meta name="fc:frame:button:4:action" content="button4_action" />
+    <meta name="fc:frame:button:4:target" content="button4_target" />
+    <meta name="fc:frame:refresh_period" content="refresh_period" />
+    <meta name="fc:frame:idem_key" content="idem_key" />
+    <meta name="fc:frame:fake_idk" content="fake" />
+    <meta name="fc:frame:input:text" content="text_input"/>
+    <meta name="fc:frame:image:aspect_ratio" content="1:1"/>
+    </head>
+    `;
+    const dom = cheerio.load(fullFrameHtml);
+    const frame = metascraperFrame();
+
+    checkRule(frame, dom, "frameVersion", "vNext");
+    checkRule(frame, dom, "frameImage", "image");
+    checkRule(frame, dom, "framePostUrl", "post_url");
+    checkRule(frame, dom, "frameButton1", "button1");
+    checkRule(frame, dom, "frameButton1Action", "button1_action");
+    checkRule(frame, dom, "frameButton1Target", "button1_target");
+    checkRule(frame, dom, "frameButton2", "button2");
+    checkRule(frame, dom, "frameButton2Action", "button2_action");
+    checkRule(frame, dom, "frameButton2Target", "button2_target");
+    checkRule(frame, dom, "frameButton3", "button3");
+    checkRule(frame, dom, "frameButton3Action", "button3_action");
+    checkRule(frame, dom, "frameButton3Target", "button3_target");
+    checkRule(frame, dom, "frameButton4", "button4");
+    checkRule(frame, dom, "frameButton4Action", "button4_action");
+    checkRule(frame, dom, "frameButton4Target", "button4_target");
+    checkRule(frame, dom, "frameRefreshPeriod", "refresh_period");
+    checkRule(frame, dom, "frameIdemKey", "idem_key");
+    checkRule(frame, dom, "frameTextInput", "text_input");
+    checkRule(frame, dom, "frameImageAspectRatio", "1:1");
   });
 });
 
