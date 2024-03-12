a55ba48bc023b7274e83e2b9128b09438695c769
Slokh <slokhx@gmail.com>
2024-03-12 00:01:09 -0400

    fix(content): worker

--- packages/farcaster/backfill/cache.ts
+++ packages/farcaster/backfill/cache.ts
@@ -18,23 +18,21 @@ const run = async () => {
       );
       nextCursor = casts.nextCursor;
       count += casts.data.length;
-
-      console.log(nextCursor);
     } while (nextCursor);
 
     console.log(`[${fid}] backfilled ${count} casts`);
   };
 
-  // const worker = getWorker(QueueName.Backfill, async (job) => {
-  //   await backfillContentForFid(job.data.fid);
-  //   console.log(`[${job.data.fid}] backfilled cache`);
-  // });
+  const worker = getWorker(QueueName.Backfill, async (job) => {
+    await backfillContentForFid(job.data.fid);
+    console.log(`[${job.data.fid}] backfilled cache`);
+  });
 
-  // worker.on("failed", (job, err) => {
-  //   if (job) {
-  //     console.log(`[${job.id}] failed with ${err.message}`);
-  //   }
-  // });
+  worker.on("failed", (job, err) => {
+    if (job) {
+      console.log(`[${job.id}] failed with ${err.message}`);
+    }
+  });
 
   await backfillContentForFid("30");
 };
