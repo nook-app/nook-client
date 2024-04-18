b5384d70b0f632dda15b3c924c80379989bd5403
Slokh <slokhx@gmail.com>
2024-04-18 08:07:53 -0400

    fix: migrate casts to priority queue

--- packages/events/handlers/index.ts
+++ packages/events/handlers/index.ts
@@ -1,6 +1,12 @@
-import { EntityEvent, EntityEventData, EventService } from "@nook/common/types";
+import {
+  EntityEvent,
+  EntityEventData,
+  EventService,
+  FarcasterEventType,
+} from "@nook/common/types";
 import { Job } from "bullmq";
 import { FarcasterProcessor } from "./farcaster";
+import { QueueName, publishEvent } from "@nook/common/queues";
 
 export const getEventsHandler = async () => {
   const farcasterProcessor = new FarcasterProcessor();
@@ -8,9 +14,16 @@ export const getEventsHandler = async () => {
   return async (job: Job<EntityEvent<EntityEventData>>) => {
     const event = job.data;
 
+    const isPriority = job.queueName === QueueName.EventsPriority;
+
     switch (event.source.service) {
       case EventService.FARCASTER: {
-        await farcasterProcessor.process(event);
+        // Move non-priority events to the priority queue
+        if (event.source.type === FarcasterEventType.CAST_ADD && !isPriority) {
+          await publishEvent(event, true);
+        } else {
+          await farcasterProcessor.process(event);
+        }
         break;
       }
       default:
