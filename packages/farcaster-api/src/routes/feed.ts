8c0e35b525c215b1ba40a5afc29d8c7801ce1a66
Slokh <slokhx@gmail.com>
2024-05-23 10:24:11 -0400

    chore(farcaster): log errors for feed requests

--- packages/farcaster-api/src/routes/feed.ts
+++ packages/farcaster-api/src/routes/feed.ts
@@ -15,48 +15,62 @@ export const feedRoutes = async (fastify: FastifyInstance) => {
       Body: FarcasterFeedRequest;
       Querystring: { cursor?: string };
     }>("/feed/casts", async (request, reply) => {
-      const response = await service.getCastFeed(request.body);
-      const casts = await farcasterService.getCastsFromData(
-        response.data,
-        request.body.context?.viewerFid,
-      );
-      reply.send({
-        data: casts,
-        nextCursor: response.nextCursor,
-      });
+      try {
+        const response = await service.getCastFeed(request.body);
+        const casts = await farcasterService.getCastsFromData(
+          response.data,
+          request.body.context?.viewerFid,
+        );
+        reply.send({
+          data: casts,
+          nextCursor: response.nextCursor,
+        });
+      } catch (e) {
+        console.error(e);
+        reply.send({
+          data: [],
+        });
+      }
     });
 
     fastify.post<{
       Body: FarcasterFeedRequest;
       Querystring: { cursor?: string };
     }>("/feed", async (request, reply) => {
-      const { newCasts, currentCasts, oldCasts } = await service.getFeed(
-        request.body,
-      );
+      try {
+        const { newCasts, currentCasts, oldCasts } = await service.getFeed(
+          request.body,
+        );
 
-      const casts = await farcasterService.getCastsFromData(
-        newCasts.concat(oldCasts),
-        request.body.context?.viewerFid,
-        currentCasts,
-      );
+        const casts = await farcasterService.getCastsFromData(
+          newCasts.concat(oldCasts),
+          request.body.context?.viewerFid,
+          currentCasts,
+        );
 
-      const sortedCasts = casts
-        .sort((a, b) => b.timestamp - a.timestamp)
-        .slice(0, MAX_PAGE_SIZE);
+        const sortedCasts = casts
+          .sort((a, b) => b.timestamp - a.timestamp)
+          .slice(0, MAX_PAGE_SIZE);
 
-      const cursor =
-        sortedCasts.length > 0
-          ? Math.min(...sortedCasts.map((cast) => cast.timestamp))?.toString()
-          : undefined;
+        const cursor =
+          sortedCasts.length > 0
+            ? Math.min(...sortedCasts.map((cast) => cast.timestamp))?.toString()
+            : undefined;
 
-      reply.send({
-        data: sortedCasts,
-        cursor: cursor
-          ? encodeCursor({
-              timestamp: cursor,
-            })
-          : undefined,
-      });
+        reply.send({
+          data: sortedCasts,
+          cursor: cursor
+            ? encodeCursor({
+                timestamp: cursor,
+              })
+            : undefined,
+        });
+      } catch (e) {
+        console.error(e);
+        reply.send({
+          data: [],
+        });
+      }
     });
   });
 };
